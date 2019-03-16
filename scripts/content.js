b392adb2bab5f9b9848072a2b130c39efd49028b
Buwaneka Thilakasiri <buwanekathilakasiri@gmail.com>
2019-03-16 21:49:29 +0530

    Issue fix not saving properly

--- scripts/content.js
+++ scripts/content.js
@@ -1,23 +1,24 @@
-var vm = this;
-
-var step = {};
+/* eslint-disable max-len */
+let step = {};
+let recording = false;
+let unsavedSteps = [];
 
 
 /* Generate XPath for UI Objects */
-const getElementXPath = element => {
+const getElementXPath = (element) => {
   if (element && element.id) return '//*[@id="' + element.id + '"]';
   else return getElementTreeXPath(element);
-}
+};
 
 /* Generate XPath for UI Objects - Advanced */
-const getElementTreeXPath = element => {
-  var paths = [];
+const getElementTreeXPath = (element) => {
+  const paths = [];
 
   // Use nodeName (instead of localName) so namespace prefix is included (if any).
   for (; element && element.nodeType == 1; element = element.parentNode) {
-    var index = 0;
+    let index = 0;
     for (
-      var sibling = element.previousSibling;
+      let sibling = element.previousSibling;
       sibling;
       sibling = sibling.previousSibling
     ) {
@@ -27,184 +28,186 @@ const getElementTreeXPath = element => {
       if (sibling.nodeName == element.nodeName) ++index;
     }
 
-    var tagName = element.nodeName.toLowerCase();
-    var pathIndex = index ? "[" + (index + 1) + "]" : "";
+    const tagName = element.nodeName.toLowerCase();
+    const pathIndex = index ? '[' + (index + 1) + ']' : '';
     paths.splice(0, 0, tagName + pathIndex);
   }
 
-  return paths.length ? "/" + paths.join("/") : null;
-}
+  return paths.length ? '/' + paths.join('/') : null;
+};
 
 /* Send a message to background script to see if theres and ongoing record */
-chrome.runtime.sendMessage({ method: "getStatus" }, isRecording => {
+chrome.runtime.sendMessage({method: 'getStatus'}, (isRecording) => {
   if (isRecording == 'true') {
     continueRecord();
   }
 });
 
 /* Listen for messages from the DOM */
-window.addEventListener("message", event => {
+window.addEventListener('message', (event) => {
   if (event.source != window) return;
 
-  if (event.data.type && event.data.type == "startRecording") {
+  if (event.data.type && event.data.type == 'startRecording') {
     record();
-  } else if (event.data.type && event.data.type == "stopRecording") {
+  } else if (event.data.type && event.data.type == 'stopRecording') {
     stopRecord();
   }
 });
 
 /* Start a new record */
 const record = () => {
+  recording = true;
+  unsavedSteps = [];
   setSteps([]);
-  console.log("Starting record");
+  console.log('Starting record');
   bindEventsToRecord();
-  chrome.runtime.sendMessage({ method: "setStatus", status: true });
+  chrome.runtime.sendMessage({method: 'setStatus', status: true});
 };
 
 /* Continue the ongoing record */
 const continueRecord = () => {
-  console.log("Continuing record");
-  bindEventsToRecord();
+  chrome.storage.sync.get(['steps'], (items) => {
+    recording = true;
+    unsavedSteps = items.steps;
+    console.log('Continuing record : STEPS : '+JSON.stringify(unsavedSteps));
+    bindEventsToRecord();
+  });
 };
 
 /* Stop the ongoing record */
 const stopRecord = () => {
+  recording = false;
   unbindEventsToRecord();
-  console.log("Record Stopped");
-  chrome.runtime.sendMessage({ method: "setStatus", status: false });
+  console.log('Record Stopped');
+  chrome.runtime.sendMessage({method: 'setStatus', status: false});
   printSteps();
 };
 
 /* Validated the click event UI Object */
-const isValidClick = el => {
+const isValidClick = (el) => {
   const tag = el.tagName.toLowerCase();
-  const type = el.getAttribute("type");
+  const type = el.getAttribute('type');
 
-  if (["a", "button"].indexOf(tag) !== -1) return true;
-  if (tag === "input" && ["button", "submit"].indexOf(type) === -1) return false;
+  if (['a', 'button'].indexOf(tag) !== -1) return true;
+  if (tag === 'input' && ['button', 'submit'].indexOf(type) === -1) return false;
   return true;
 };
 
 /* Validate the Select event UI Object */
-const isValidSelect = el => {
+const isValidSelect = (el) => {
   const tag = el.tagName.toLowerCase();
 
-  if (["option", "select"].indexOf(tag) !== -1) return true;
+  if (['option', 'select'].indexOf(tag) !== -1) return true;
   return false;
 };
 
 /* Validate the type event UI Object */
-const isValidType = el => {
+const isValidType = (el) => {
   const tag = el.tagName.toLowerCase();
-  const type = el.getAttribute("type");
+  // const type = el.getAttribute('type');
 
-  if (tag === "textarea") return true;
-  if (tag === "input") return true;
+  if (tag === 'textarea') return true;
+  if (tag === 'input') return true;
 
   return false;
 };
 
-const isValidKeyPress = e => {
-  var keycode = e.keyCode;
-  var el = e.target;
+const isValidKeyPress = (e) => {
+  const keycode = e.keyCode;
+  const el = e.target;
   if ((keycode >= 48 && keycode <=57) || (keycode >= 65 && keycode <= 90)) return false;
-  if(keycode === 13 && el.tagName.toLowerCase() == "input") return false;
+  if (keycode === 13 && el.tagName.toLowerCase() == 'input') return false;
   return true;
 };
 
-const onClick = e => {
+const onClick = (e) => {
   if (!isValidClick(e.target)) return;
-  var targetElement = event.target || event.srcElement;
+  const targetElement = event.target || event.srcElement;
   step = {};
-  console.log("click on " + getElementXPath(targetElement));
-  step.type = "click";
+  console.log('click on ' + getElementXPath(targetElement));
+  step.type = 'click';
   step.xpath = getElementXPath(targetElement);
   saveStep(step);
 };
 
-const onChange = e => {
+const onChange = (e) => {
   if (isValidSelect(e.target)) {
-    var targetElement = event.target || event.srcElement;
+    const targetElement = event.target || event.srcElement;
     console.log(
-      "select " + getDropdownSelectedOption() + " of " + getElementXPath(targetElement)
+        'select ' + getDropdownSelectedOption() + ' of ' + getElementXPath(targetElement)
     );
     step = {};
-    step.type = "select";
+    step.type = 'select';
     step.displayText = getDropdownSelectedOption();
     step.xpath = getElementXPath(targetElement);
     saveStep(step);
-
   } else if (isValidType(e.target)) {
-
-    const value = (e.target.value || "").replace(/\n/g, "\\n");
-    var targetElement = event.target || event.srcElement;
-    console.log("type " + value + " on " + getElementXPath(targetElement));
+    const value = (e.target.value || '').replace(/\n/g, '\\n');
+    const targetElement = event.target || event.srcElement;
+    console.log('type ' + value + ' on ' + getElementXPath(targetElement));
     step = {};
-    step.type = "type";
+    step.type = 'type';
     step.value = value;
     step.xpath = getElementXPath(targetElement);
     saveStep(step);
   }
 };
 
-const onKeyPress = e => {
-  if(!isValidKeyPress(e)) return;
-  var targetElement = event.target || event.srcElement;
+const onKeyPress = (e) => {
+  if (!isValidKeyPress(e)) return;
+  const targetElement = event.target || event.srcElement;
   console.log(
-    "press " + e.keyCode + " on " + getElementXPath(targetElement)
+      'press ' + e.keyCode + ' on ' + getElementXPath(targetElement)
   );
   step = {};
-  step.type = "press";
+  step.type = 'press';
   step.keycode = e.keyCode;
   step.xpath = getElementXPath(targetElement);
   saveStep(step);
 };
 
-const getCheckBoxSelectedState = e => {
-  return e.target.checked;
-};
+// const getCheckBoxSelectedState = (e) => {
+//   return e.target.checked;
+// };
 
-const getDropdownSelectedOption = e => {
+const getDropdownSelectedOption = (e) => {
   const value = e.target.value;
   const $option = Array.from(e.target.children).find(
-    $op => $op.value === value
+      ($op) => $op.value === value
   );
   return $option.text.trim();
 };
 
 const bindEventsToRecord = () => {
-  document.addEventListener("click", onClick, true);
-  document.addEventListener("change", onChange, true);
-  document.addEventListener("keyup", onKeyPress, true);
+  document.addEventListener('click', onClick, true);
+  document.addEventListener('change', onChange, true);
+  document.addEventListener('keyup', onKeyPress, true);
 };
 
 const unbindEventsToRecord = () => {
-  document.removeEventListener("click", onClick, true);
-  document.removeEventListener("change", onChange, true);
-  document.removeEventListener("keyup", onKeyPress, true);
+  document.removeEventListener('click', onClick, true);
+  document.removeEventListener('change', onChange, true);
+  document.removeEventListener('keyup', onKeyPress, true);
 };
 
-const saveStep = step => {
-  console.log("Saving step "+JSON.stringify(step));
-  chrome.storage.sync.get(["steps"], function(items) {
-    var stepsList = items.steps;
-    stepsList.push(step);
-    chrome.storage.sync.set({ steps: stepsList });
-    console.log("Saved step "+JSON.stringify(step));
-  });
+const saveStep = (step) => {
+  unsavedSteps.push(step);
 };
 
-const setSteps = stepsList => {
-  chrome.storage.sync.set({ steps: stepsList });
+const setSteps = (stepsList) => {
+  chrome.storage.sync.set({steps: stepsList});
 };
 
 const printSteps = () => {
-  chrome.storage.sync.get(["steps"], function(items) {
-  console.log("Steps : \n" + JSON.stringify(items.steps));
+  chrome.storage.sync.get(['steps'], function(items) {
+    console.log('Steps : \n' + JSON.stringify(items.steps));
   });
 };
 
-window.onbeforeunload = event => {
-  if (!finished)
-    chrome.runtime.sendMessage({ method: "setStatus", status: "recording" });
+window.onbeforeunload = (event) => {
+  if (recording) {
+    chrome.storage.sync.set({steps: unsavedSteps}, () => {
+      console.log('Uploaded : STEPS : '+JSON.stringify(unsavedSteps));
+    });
+  }
 };
